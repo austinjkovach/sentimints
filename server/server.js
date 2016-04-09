0df70f8c21a7f3af4a90841149742beb3a020f39
austinjkovach <kovach.a.j@gmail.com>
2016-04-09 13:24:37 -0500

    Server is running

--- server/server.js
+++ server/server.js
@@ -3,10 +3,14 @@ var path = require('path');
 
 var app = express();
 var port = 3000;
-var publicPath = path.resolve(__dirname, 'client');
+var publicPath = path.resolve(__dirname + '/../client/public');
 
 app.use(express.static(publicPath));
 
+app.get('/*', function(req, res){
+  res.sendFile(publicPath + '/index.html')
+});
+
 app.listen(port, function(){
   console.log('Listening on port', port);
-})
+});
