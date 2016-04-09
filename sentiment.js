cd59d6a49f9b2593fade87f339772e814ca8122f
Marta Johnson <marta.hourigan.johnson@gmail.com>
2016-04-09 15:32:08 -0500

    planning what to show

--- sentiment.js
+++ sentiment.js
@@ -1,30 +1,19 @@
 "use strict"
 
-// const HTTPS = require('https');
 const FETCH = require('node-fetch');
 FETCH.promise = require('bluebird');
 
 const Promise = require('bluebird');
 const hod = require('havenondemand');
 const api = process.env['SENTIM'];
-console.log('api key', api)
 
 const client = new hod.HODClient(api); //version is optional second argument - right now I don't see a need
 
-// const url = 'https://api.havenondemand.com/1/api/async/analyzesentiment/v1';
 const resultUrl = 'https://api.havenondemand.com/1/job/result/';
 const API_URL = '?apikey=' + api;
 
-// let data = {'text': 'I fucking hate cats'};
 
-// client.call('analyzesentiment', data, true, function(err, resp, body) {
-// 	console.log('err', err);
-// 	// console.log('resp', resp);
-// 	console.log('body', body);
-// 	let jobId = body.data.jobID;
-// 	console.log('jobId, ', jobId)
 
-// })
 
 /*
   Takes a job ID
@@ -47,8 +36,10 @@ let getCompletedJob = function(jobId) {
 	  })
 }
 
+
+
 /*
-  Takes a text string (ie a review)
+  Takes a text string (ie a review, eg 'This restaurant sucks and their chips are crappy')
   Returns the result from the sentiment analysis (the full object)
 */
 let analyzeText = function(textString) {
@@ -78,26 +69,42 @@ let analyzeText = function(textString) {
 	})
 }
 
-// let myText = 'Cats are stupid and dogs are awesome.'
 
-// return analyzeText(myText)
-//   .then(function(got) {
-//   	console.log('got this from analyzeText', got);
-//   })
 
+/*
+  Take one review
+  Returns a single array of all sentiments for that review, eg:
+	   {
+	        "sentiment": "awesome",
+	        "topic": "dogs",
+	        "score": 0.8952832264587852,
+	        "original_text": "dogs are awesome",
+	        "original_length": 16,
+	        "normalized_text": "dogs are awesome",
+	        "normalized_length": 16
+	    }
+*/
 
 
 
 /*
-var data = {'text' : 'I like cats'}
-let data = {'file' : 'test.txt'}
+Aggregate (by review)
+- Summary stats
+- Summary stats for positive
+- Summary stats for negative
 
-client.call('analyzesentiment', callback, data)
+Count of positive vs negative reviews (based on overall thing)
+Hot topics - most frequent topics
+Summary statistics by topic
+Shows lowest quarter by 
 
-var callback = function(err,resp,body){
-  console.log(body)
-}
-
-client.call('analyzesentiment', data, callback, true)
+By topic:
+- Most postive topics (bottom 25th)
+- Most negative topics
+- Hottest (recency + frequency)
 
 */
+
+
+
+
