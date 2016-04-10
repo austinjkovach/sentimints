6a4d872419d9048eac19d9088064614fb8c1ec75
Julia Brown <juliacelestebrown@gmail.com>
2016-04-09 19:40:33 -0500

    more ajax

--- client/actions/userActions.jsx
+++ client/actions/userActions.jsx
@@ -1,6 +1,7 @@
 class UserActions {
-  getAllReviews() {
-    $.ajax({ url: '/api/' })
+  getAllReviews(name) {
+    let uri = '/api/get?business_name=' + name
+    $.ajax({ url: uri })
       .done((data) => {
         console.log('GOT DATA!', data);
         return data;
@@ -11,11 +12,9 @@ class UserActions {
       });
   }
 
-  getSomeReviews(data) {
-    $.ajax({ 
-      type: 'POST',
-      url: '/api/',
-      data: data})
+  getReviewsByStars(name, stars) {
+    let uri = '/api/get?business_name=' + name + '&business_stars=' + stars[0] + '_' + stars[1];
+    $.ajax({url: uri})
       .done((data) => {
         console.log('GOT DATA!', data);
         return data;
@@ -25,6 +24,20 @@ class UserActions {
         throw err;
       });
   }
+
+  getReviewsByDate(name, dateRange) {
+    let uri = '/api/get?business_name=' + name + '&business_dates=' + dateRange[0] + '_' + dateRange[1];
+    $.ajax({url: uri})
+      .done((data) => {
+        console.log('GOT DATA!', data);
+        return data;
+      })
+      .fail((err) => {
+        console.error("Didn't get data :(", err);
+        throw err;
+      });
+  }
+
 }
 
 export default UserActions;
