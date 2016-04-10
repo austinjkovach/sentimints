1971ebf94160cd8644b54d6d58cbda8b6acae27d
Julia Brown <juliacelestebrown@gmail.com>
2016-04-09 20:59:40 -0500

    calls sentiment file

--- server/review-api.js
+++ server/review-api.js
@@ -2,6 +2,7 @@ var express = require('express')
 
 var Promise = require('bluebird')
 var db = require('./db-helpers')
+var sentiments = require('./sentiments.js');
 
 console.log('we are in review-api')
 
@@ -12,7 +13,14 @@ MintAPI.get('/', function(req, res) {
   var business_name = req.query.business_name + ''
   db.selectAllReviews(business_name)
   .then(function(reviews){
-    res.send(reviews)
+    return sentiments.getAllFromReviews(reviews);
+  })
+  .then(function(sentiments){
+    res.send(sentiments)
+  })
+  .catch(function(err){
+    console.error('OH NO. SENTIMENT ERROR', err);
+    res.status(500).send(err);
   })
 })
 
@@ -21,7 +29,14 @@ MintAPI.get('/stars', function(req, res) {
   var stars = req.query.business_stars.split('_')
   db.selectByStars(business_name, stars)
   .then(function(reviews){
-    res.send(reviews)
+    return sentiments.getAllFromReviews(reviews)
+  })
+  .then(function(sentiments){
+    res.send(sentiments)
+  })
+  .catch(function(err){
+    console.error('OH NO. SENTIMENT ERROR', err);
+    res.status(500).send(err);
   })
 })
 
@@ -30,6 +45,13 @@ MintAPI.get('/date', function(req, res) {
   var dates = req.query.business_date.split('_')
   db.selectByDate(business_name, dates)
   .then(function(reviews){
-    res.send(reviews)
+    return sentiments.getAllFromReviews(reviews)
+  })
+  .then(function(sentiments){
+    res.send(sentiments)
+  })
+  .catch(function(err){
+    console.error('OH NO. SENTIMENT ERROR', err);
+    res.status(500).send(err);
   })
 })
