a6262aa0f78f0a3fc5afccf8d61e507f33511e36
Mike Mitchel <mike@mikemitchel.com>
2016-04-09 20:48:59 -0500

    api end points working with query strings

--- server/review-api.js
+++ server/review-api.js
@@ -8,26 +8,27 @@ console.log('we are in review-api')
 var MintAPI = module.exports = express.Router()
 //{ mergeParams: true }
 
-
 MintAPI.get('/', function(req, res) {
-  console.log('params', req.query)
-  db.selectAllReviews(req.business_name)
+  var business_name = req.query.business_name + ''
+  db.selectAllReviews(business_name)
   .then(function(reviews){
     res.send(reviews)
   })
 })
 
 MintAPI.get('/stars', function(req, res) {
-  console.log('params', req.query)
-  db.selectAllReviews(req.business)
+  var business_name = req.query.business_name + ''
+  var stars = req.query.business_stars.split('_')
+  db.selectByStars(business_name, stars)
   .then(function(reviews){
     res.send(reviews)
   })
 })
 
 MintAPI.get('/date', function(req, res) {
-  console.log('params', req.query)
-  db.selectAllReviews(req.business)
+  var business_name = req.query.business_name + ''
+  var dates = req.query.business_date.split('_')
+  db.selectByDate(business_name, dates)
   .then(function(reviews){
     res.send(reviews)
   })
